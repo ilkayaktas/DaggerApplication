1ac88053209bfa21f6facb7b22f8bd46256a73ed
iaktas <ilkayaktas@gmail.com>
2017-02-10 17:36:40 +0200

    Varsayılan activity eklendi. Dagger generated files eklendi.

--- app/src/main/java/com/ilkayaktas/daggerapplication/Dagger2Application.java
+++ app/src/main/java/com/ilkayaktas/daggerapplication/Dagger2Application.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.ilkayaktas.daggerapplication.injectors.AppComponent;
 import com.ilkayaktas.daggerapplication.injectors.AppModule;
+import com.ilkayaktas.daggerapplication.injectors.DaggerAppComponent;
 
 /**
  * Created by iaktas on 09.02.2017.
